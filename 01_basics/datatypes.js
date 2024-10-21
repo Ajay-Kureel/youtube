c9816352ff6452b85fd438004566f29941f2c129
Ajay-Kureel <73892679+Ajay-Kureel@users.noreply.github.com>
2024-10-21 12:53:11 +0000

    dataypes learned

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as viewer version
+
+//alert(3+3); //we are using nodejs not browser
+
+console.log(3
+    + 
+    3
+); //code readibility should be high
+console.log("ajay");
+
+
+
+let name = "Anam";
+let age = 18;
+let isLoggedIn = false;   //boolean
+let state;
+
+//number => 2 to power 53
+//bigint => 
+//string => " "
+//boolean => true / false
+//null => standalone value
+//undefined
+//symbol => unique
+
+
+//object
+console.log(typeof(undefined)); //undefined
+console.log(typeof(null)); //object
